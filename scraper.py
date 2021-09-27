84568f068b5a1fc025d31754636e1de0f2179fca
Esben Rask Bach <erb18@student.aau.dk>
2021-09-27 12:12:41 +0200

    Update scraper.py
    added crawler for etilbudsavis and scraper that gets names of dealers from Json @bernardperru

--- scraper.py
+++ scraper.py
@@ -3,21 +3,44 @@ from bs4 import BeautifulSoup
 from time import sleep
 from urllib.robotparser import RobotFileParser
 
+def skraper(url):
+    rp.set_url(url)
+    rp.read()
+
+    r=requests.get(url)
+
+    r_parse = BeautifulSoup(r.text, 'html.parser')
+
+    data = json.loads(r_parse.find('script', type='application/json', id='__NEXT_DATA__').text)
+
+    for i in range (len(data['props']['reactQueryState']['queries'])):
+        if (data['props']['reactQueryState']['queries'][i]['state']['data']['ern'].split(':')[1] == 'offer'):
+            print(data['props']['reactQueryState']['queries'][i]['state']['data']['offer']['heading'])
+
+
+def etellerandet(url):
+    rp.set_url(url)
+    rp.read()
+    
+    r=requests.get(url)
+    r_parse = BeautifulSoup(r.text, "html.parser")
+    data = json.loads(r_parse.find('script', type='application/json', id='__NEXT_DATA__').text)
+
+    for i in range (len(data['props']['reactQueryState']['queries'])):
+        if ('ern' in data['props']['reactQueryState']['queries'][i]['state']['data']):
+            if (data['props']['reactQueryState']['queries'][i]['state']['data']['ern'].split(':')[1] == 'catalog'):
+                skraper('https://etilbudsavis.dk/business/{dealerid}/publications/{catalogid}/paged'.format(
+                    dealerid = data['props']['reactQueryState']['queries'][i]['state']['data']['dealerId'],
+                    catalogid = data['props']['reactQueryState']['queries'][i]['state']['data']['id']
+                ))
+                print(data['props']['reactQueryState']['queries'][i]['state']['data']['branding']['name'])
+
+
 rp=RobotFileParser()
 
-urllink = "https://etilbudsavis.dk/businesses/101cD/publications/FzKCNPup/paged"
+urllink = "https://etilbudsavis.dk"
 
-rp.set_url(urllink)
-rp.read()
-print(rp.can_fetch("*", urllink))
+etellerandet(urllink)
 
-r=requests.get(urllink)
-
-r_parse = BeautifulSoup(r.text, 'html.parser')
-
-data = json.loads(r_parse.find('script', type='application/json', id='__NEXT_DATA__').text)
-
-for x in data['props']['reactQueryState']['queries'][3]['state']['data']:
-    print(x['offer']['heading'])
 
 
