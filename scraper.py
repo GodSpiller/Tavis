f457f0e1a78c96dd6e146cac67b34a68a958a434
bernardperru <47976460+bernardperru@users.noreply.github.com>
2021-09-29 10:25:57 +0200

    Update scraper.py
    Gjort rent + det virker :+1:

--- scraper.py
+++ scraper.py
@@ -6,37 +6,14 @@ from urllib.robotparser import RobotFileParser
 from requests.models import Response
 
 
-def scraper2(catalogId):
-    print(catalogId)
+def scraper(catalogId):
     link = "https://etilbudsavis.dk/api/squid/v2/catalogs/{catId}/hotspots".format(
         catId = catalogId
     )
-
-    response = json.loads(requests.get(link))
+    response = json.loads(requests.get(link).text)
 
     for offer in response:
-        print("   " + offer['Heading'])
-
-def skraper(storeID, catId):
-
-    url = 'https://etilbudsavis.dk/businesses/{dealerid}/publications/{catalogid}/paged'.format(
-        dealerid = storeID,
-        catalogid = catId
-    )
-
-    rp.set_url(url)
-    rp.read()
-
-    r=requests.get(url)
-
-    r_parse = BeautifulSoup(r.text, 'html.parser')
-
-    data = json.loads(r_parse.find('script', type='application/json', id='__NEXT_DATA__').text)
-
-    for i in data['props']['reactQueryState']['queries']:
-        if ('ern' in i['state']['data']):
-            if(i['state']['data']['ern'].split(":")[1] == "offer"):
-                print("    " + i['state']['data']['heading'])
+        print(offer['offer']['pricing']['price'])
 
 def getAllCatalogs(url):
     rp.set_url(url)
@@ -55,19 +32,7 @@ def getAllCatalogs(url):
                     storeId = i['state']['data']['dealerId'],
                     catId = i['state']['data']['id']
                 ))
-                scraper2(i['state']['data']['id'])
-    
-    """
-    for i in range (len(data['props']['reactQueryState']['queries'])):
-
-        if ('ern' in data['props']['reactQueryState']['queries'][i]['state']['data']):
-            if (data['props']['reactQueryState']['queries'][i]['state']['data']['ern'].split(':')[1] == 'catalog'):
-                skraper('https://etilbudsavis.dk/business/{dealerid}/publications/{catalogid}/paged'.format(
-                    dealerid = data['props']['reactQueryState']['queries'][i]['state']['data']['dealerId'],
-                    catalogid = data['props']['reactQueryState']['queries'][i]['state']['data']['id']
-                ))
-                print(data['props']['reactQueryState']['queries'][i]['state']['data']['branding']['name'])
-    """
+                scraper(i['state']['data']['id'])
 
 
 rp=RobotFileParser()
