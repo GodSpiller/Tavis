d06003ff1d2008d7118e20c983344eae1bf36fac
bernardperru <47976460+bernardperru@users.noreply.github.com>
2021-09-29 09:48:55 +0200

    123

--- scraper.py
+++ scraper.py
@@ -3,23 +3,9 @@ from bs4 import BeautifulSoup
 from time import sleep
 from urllib.robotparser import RobotFileParser
 
-from requests.models import Response
-
-
-def scraper2(catalogId):
-    link = "https://etilbudsavis.dk/api/squid/v2/catalogs/{catId}/hotspots".format(
-        catId = catalogId
-    )
-
-    response = json.loads(requests.get(link))
-
-    for offer in response:
-        print("   " + offer['Heading'])
-
-
 def skraper(storeID, catId):
 
-    url = 'https://etilbudsavis.dk/business/{dealerid}/publications/{catalogid}/paged'.format(
+    url = 'https://etilbudsavis.dk/businesses/{dealerid}/publications/{catalogid}/paged'.format(
         dealerid = storeID,
         catalogid = catId
     )
@@ -38,15 +24,15 @@ def skraper(storeID, catId):
             if(i['state']['data']['ern'].split(":")[1] == "offer"):
                 print("    " + i['state']['data']['heading'])
 
-def etellerandet(url):
+def getAllCatalogs(url):
     rp.set_url(url)
     rp.read()
     
     r=requests.get(url)
     r_parse = BeautifulSoup(r.text, "html.parser")
-    print(r_parse)
     data = json.loads(r_parse.find('script', type='application/json', id='__NEXT_DATA__').text)
 
+  
     for i in data['props']['reactQueryState']['queries']:
         if ('ern' in i['state']['data']):
             if (i['state']['data']['ern'].split(":")[1] == 'catalog'):
@@ -55,8 +41,7 @@ def etellerandet(url):
                     storeId = i['state']['data']['dealerId'],
                     catId = i['state']['data']['id']
                 ))
-                #scraper2(i['state']['data']['id'])
-                #skraper(i['state']['data']['dealerId'], i['state']['data']['id'])
+                skraper(i['state']['data']['dealerId'], i['state']['data']['id'])
     
     """
     for i in range (len(data['props']['reactQueryState']['queries'])):
@@ -75,4 +60,4 @@ rp=RobotFileParser()
 
 urllink = "https://etilbudsavis.dk/discover/groceries"
 
-etellerandet(urllink)
+getAllCatalogs(urllink)
