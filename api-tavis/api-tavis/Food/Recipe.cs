200852dd0bde7b9227304128a80fd30d8bfee83a
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2021-10-08 17:20:34 +0200

    Create Recipe.cs

---
+++ api-tavis/api-tavis/Food/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api_tavis.Food
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public string Guide { get; set; }
+
+        public List<Ingredient> Ingredients { get; set; }
+    }
+}
