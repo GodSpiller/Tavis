3c445d26e50a70111abeed7abda800a22dc099fa
DeagleBiegel <47975952+DeagleBiegel@users.noreply.github.com>
2021-10-11 20:14:51 +0200

    Changed width collapse

--- react-tavis/src/components/NavBar.js
+++ react-tavis/src/components/NavBar.js
@@ -16,9 +16,9 @@ import {Link} from 'react-router-dom'
 function NavBar () {
     
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">T-AVIS</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
@@ -34,9 +34,9 @@ function NavBar () {
                     </NavDropdown>
                     </Nav>
                     <Nav>
-                    <Nav.Link href="#deets">More deets</Nav.Link>
-                    <Nav.Link eventKey={2} href="#memes">
-                        Dank memes
+                    <Nav.Link>Register</Nav.Link>
+                    <Nav.Link>
+                        Login
                     </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
