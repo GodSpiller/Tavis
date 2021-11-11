dba478a1b7ee4b52c72d1de00cf86e39c4d2cd12
bernardperru <47976460+bernardperru@users.noreply.github.com>
2021-11-11 09:13:40 +0100

    Fixet biegels semikolonner

--- frida.py
+++ frida.py
@@ -7,8 +7,8 @@ df = pd.read_excel(xl_file)
 
 # Inserts data into food_supercategory with the group name
 def insertSuperCategories():
-    conn = database.connectToDB();
-    curs = conn.cursor();
+    conn = database.connectToDB()
+    curs = conn.cursor()
 
     alreadyUsed = []
 
@@ -26,8 +26,8 @@ def insertSuperCategories():
 # Inserts data to the food_category table with the name of the products
 # and the foreign key corresponding to the group
 def insertCategories():
-    conn = database.connectToDB();
-    curs = conn.cursor();
+    conn = database.connectToDB()
+    curs = conn.cursor()
 
     for index, row in df.iterrows():
         group = row['gruppe']
